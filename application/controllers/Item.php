fb54b1ec9350bd9bc80ebe9dcdcced95d66c3064
klkuo <david@itaifeng.com>
2016-12-06 16:19:13 +0800

    Add controlles file

---
+++ application/controllers/Item.php
@@ -0,0 +1,37 @@
+<?php
+class Item extends CI_Controller
+{
+    /**
+     * Constructor 
+     *
+     * @author david.kuo <davidkuo104@gmail.com>
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function add()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function get()
+    {
+    }
+
+    public function listing()
+    {
+    }
+
+    public function action_add()
+    {
+    }
+
+    public function action_modify()
+    {
+    }
+}
---
+++ application/controllers/Member.php
@@ -0,0 +1,37 @@
+<?php
+class Member extends CI_Controller
+{
+    /**
+     * Constructor 
+     *
+     * @author david.kuo <davidkuo104@gmail.com>
+     */
+    public function __construct()
+    {
+        Parent::__construct();
+    }
+
+    public function add()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function get()
+    {
+    }
+
+    public function listing()
+    {
+    }
+
+    public function action_add()
+    {
+    }
+
+    public function action_modify()
+    {
+    }
+}
